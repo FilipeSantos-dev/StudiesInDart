5c62c88869c597912278a8d3eb05bb3ff675f563
FilipeSantos-dev <filipe.santos0330@gmail.com>
2020-11-17 03:36:35 -0300

    Algoritimos basicos

---
+++ Aula02/calculos.dart
@@ -0,0 +1,7 @@
+main() {
+  int altura = 2;
+  double peso = 70.0;
+  num total = altura * peso;
+
+  print(total);
+}
---
+++ Aula03/concatenandoStrings.dart
@@ -0,0 +1,8 @@
+main() {
+  String nome = "Filipe";
+  String sobrenome = "José";
+
+  String nome_completo = "$nome $sobrenome dos Santos";
+
+  print(nome_completo);
+}
