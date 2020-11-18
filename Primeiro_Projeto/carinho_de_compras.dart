57df8dd36c0e3f697d8bc06f32a64923c638b41f
FilipeSantos-dev <filipe.santos0330@gmail.com>
2020-11-18 18:19:53 -0300

    Projeto Finalizado

--- Primeiro_Projeto/carinho_de_compras.dart
+++ Primeiro_Projeto/carinho_de_compras.dart
@@ -10,14 +10,19 @@ main() {
   while(condicao) {
     print("=== Informe o nome do produto ou oque deseja:");
     String texto = stdin.readLineSync();
+    print("\x1B[2J\x1B[0;0H");
 
     if(texto == "sair") {
       sair();
       condicao = false;
-    }else if(texto == "carrinho") {
+    }else if(texto == "imprimir") {
       carrinho(produtos);
+    } else if(texto == "remover") {
+      carrinho(produtos);
+      produtos.removeAt(remover(produtos));
     } else {
       produtos.add(texto);
+      print("\x1B[2J\x1B[0;0H");
     }
   }
 }
@@ -30,16 +35,28 @@ introducao() {
   print("");
 }
 
+// função que ira exibir os produtos já cadastrados
+String carrinho(produtos) {
+  if(produtos == null) {
+    print("Nenhum produto foi adicionado ao carrinho ainda.");
+  } else {
+    print("====== LISTA DE PRODUTOS ======");
+
+    for(int i = 0; i < produtos.length; i++) {
+      print("Item $i - ${produtos[i]}.");
+    }
+  }
+}
+
+// função que ira remover o item selecionado pelo usuarios
+int remover(produtos) {
+  print("=== Informe qual item deseja remover:");
+  int item = int.parse(stdin.readLineSync());
+  
+  return item;
+}
+
 // função que ira finalizar o programa
 void sair() {
   print("====== PROGRAMA FINALIZADO ======");
-}
-
-// função que ira exibir os produtos já cadastrados
-String carrinho(produtos) {
-  print("====== LISTA DE PRODUTOS ======");
-
-  for(int i = 0; i < produtos.length; i++) {
-    print("Produto $i: " + produtos[i] + ".");
-  }
 }
